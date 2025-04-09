238b22e81c8c0cf0876d3fa000bd47abff7104ce
petya3 <64072044+petya3@users.noreply.github.com>
2025-04-09 20:13:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ import numpy as np
 
 size_of_board = 600
 symbol_size = (size_of_board / 3 - size_of_board / 8) / 2
-symbol_thickness = 50
+symbol_thickness = 70
 symbol_X_color = '#EE4035'
 symbol_O_color = '#0492CF'
 Green_color = '#7BC043'
-
+#c!
 
 class Tic_Tac_Toe():
     # ------------------------------------------------------------------
